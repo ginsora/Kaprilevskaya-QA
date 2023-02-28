f7d161709e9fa46a782bea7972422e58b64c7333
Valeria Kaprilevskaya <kaprilevskaya.valeria@gmail.com>
2023-02-28 22:02:28 +0700

    Adding second task

---
+++ Practice_3/src/test/java/SecondTest.java
@@ -0,0 +1,90 @@
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.time.Duration;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(DataProviderRunner.class)
+
+public class SecondTest {
+    private WebDriver driver;
+    private String baseUrl;
+
+    @Before
+    public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "src/test/chromedriver.exe");
+        driver = new ChromeDriver();
+        baseUrl = "https://sandbox.cardpay.com/MI/cardpayment2.html?orderXml=PE9SREVSIFdBTExFVF9JRD0nODI5OScgT1JERVJfTlVNQkVSPSc0NTgyMTEnIEFNT1VOVD0nMjkxLjg2JyBDVVJSRU5DWT0nRVVSJyAgRU1BSUw9J2N1c3RvbWVyQGV4YW1wbGUuY29tJz4KPEFERFJFU1MgQ09VTlRSWT0nVVNBJyBTVEFURT0nTlknIFpJUD0nMTAwMDEnIENJVFk9J05ZJyBTVFJFRVQ9JzY3NyBTVFJFRVQnIFBIT05FPSc4NzY5OTA5MCcgVFlQRT0nQklMTElORycvPgo8L09SREVSPg==&sha512=998150a2b27484b776a1628bfe7505a9cb430f276dfa35b14315c1c8f03381a90490f6608f0dcff789273e05926cd782e1bb941418a9673f43c47595aa7b8b0d";
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(60));
+    }
+
+    @DataProvider   // Ввод кучи данных
+    public static Object[][] data() {
+        return new Object[][] {
+                {"4000000000000002", "JOHN DOE", "02", "2030", "123", "Success", "Confirmed", "VISA"},
+                //{"5555555555554444", "DECLINED BY ISSUING BANK"}
+        };
+    }
+
+
+    @Test
+    @UseDataProvider("data")
+    public void finalPageTesting(String cardNum, String cardHolder, String cardMonth, String cardYear, String cardCvc, String operationStatus, String paymentStatus, String cardType) {
+        driver.get(baseUrl);
+        String Order_number = (String) driver.findElement(By.id("order-number")).getText();
+        String Total = (String) driver.findElement(By.id("total-amount")).getText();
+        String Currency = (String) driver.findElement(By.id("currency")).getText();
+        driver.findElement(By.id("input-card-number")).click();
+        driver.findElement(By.id("input-card-number")).clear();
+        driver.findElement(By.id("input-card-number")).sendKeys(cardNum);
+        driver.findElement(By.id("input-card-holder")).click();
+        driver.findElement(By.id("input-card-holder")).clear();
+        driver.findElement(By.id("input-card-holder")).sendKeys(cardHolder);
+        driver.findElement(By.id("card-expires-month")).click();
+        new Select(driver.findElement(By.id("card-expires-month"))).selectByVisibleText(cardMonth);
+        driver.findElement(By.id("card-expires-year")).click();
+        new Select(driver.findElement(By.id("card-expires-year"))).selectByVisibleText(cardYear);
+        driver.findElement(By.id("input-card-cvc")).click();
+        driver.findElement(By.id("input-card-cvc")).clear();
+        driver.findElement(By.id("input-card-cvc")).sendKeys(cardCvc);
+        driver.findElement(By.id("action-submit")).click();
+        driver.findElement(By.id("success")).click();
+
+
+        // Сравнение статуса операции
+        assertEquals(operationStatus, driver.findElement(By.xpath("//*[@id=\"payment-status-title\"]/span")).getText());
+
+        // Сравнение номера заказа
+        assertEquals(Order_number, driver.findElement(By.xpath("//*[@id=\"payment-item-ordernumber\"]/div[2]")).getText());
+
+        // Сравнение статуса платежа
+        assertEquals(paymentStatus, driver.findElement(By.xpath("//*[@id=\"payment-item-status\"]/div[2]")).getText());
+
+        // Сравнение типа карты
+        assertEquals(cardType, driver.findElement(By.xpath("//*[@id=\"payment-item-cardtype\"]/div[2]")).getText());
+
+        // Сравнение держателя карты
+        assertEquals(cardHolder, driver.findElement(By.xpath("//*[@id=\"payment-item-cardholder\"]/div[2]")).getText());
+
+        // Сравнение валюты
+        assertEquals(Currency + "   " + Total, driver.findElement(By.xpath("//*[@id=\"payment-item-total\"]/div[2]")).getText());
+
+        // Сравнение суммы
+        assertEquals(Total, driver.findElement(By.xpath("//*[@id=\"payment-item-total-amount\"]")).getText());
+    }
+
+    @After
+    public void tearDown() {
+        driver.quit();
+    }
+}
